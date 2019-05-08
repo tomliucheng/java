af0d55903a4bd5272026aa2bd399177babcb345a
liucheng <986435781@qq.com>
2019-05-08 16:08:44 +0800

    搜集spring boot的各种注解及其一手解释，方便成功使用该框架

---
+++ annotations.java
@@ -0,0 +1,17 @@
+@Id
+@GeneratedValue(strategy=GenerationType.AUTO)
+//JPA will recognize it as the object’s ID
+//indicate that the ID should be generated automatically
+
+@Entity
+//indicate this is a JAP entity
+
+@SpringBootApplication
+//is a convenience annotation that adds all of the following
+
+@Configuration // tags the class as a source of bean definitions for the application context.
+@EnableAutoConfiguration 
+//tells Spring Boot to start adding beans based on classpath settings, other beans, and various property settings.
+@ComponentScan 
+//tells Spring to look for other components, configurations, and services in the hello package, allowing it to find the controllers.
+
